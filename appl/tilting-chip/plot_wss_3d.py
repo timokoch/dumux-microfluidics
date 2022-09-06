abdfd16219ab69923c189152311af11b01003b93
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2022-09-06 10:03:26 +0200

    Update run/plot scriprs

--- appl/tilting-chip/plot_wss_3d.py
+++ appl/tilting-chip/plot_wss_3d.py
@@ -15,8 +15,8 @@ X, Y = np.meshgrid(tilting_speeds, angles)
 def max_wss(speed, tilt):
     def generate():
         for s, t in zip(speed, tilt):
-            file_name = f"wss_s{s}_t{t}_p0_300ul_d500_300_big.txt"
-            #file_name = f"wss_s{s}_t{t}_p0_200ul_d500_300_small.txt"
+            #file_name = f"wss_s{s}_t{t}_p0_300ul_d500_big.txt"
+            file_name = f"wss_s{s}_t{t}_p0_200ul_d500_small.txt"
             print(f"Parse {file_name}")
             if Path(file_name).is_file():
                 sim = np.genfromtxt(file_name, delimiter=" ", skip_header=1, skip_footer=2)
@@ -32,10 +32,12 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
-ax.set_title('Maximum WSS (outer circuit | 300µl)')
+#ax.set_title('Maximum WSS (outer circuit | 300µl)')
+ax.set_title('Maximum WSS (inner circuit | 200µl)')
 ax.set_xlabel('tilting speed (rpm)')
 ax.set_ylabel('tilt angle (degree)')
 ax.set_zlabel(r'Maximum WSS (dyne/cm$^{2}$)')
+ax.set_zlim(0, 8)
 fig.tight_layout()
 plt.show()
 fig.savefig("wss_wireframe.pdf", dpi=600)
--- appl/tilting-chip/run_validation.py
+++ appl/tilting-chip/run_validation.py
@@ -12,9 +12,9 @@ tilting_speeds = ["3", "5", "8"]
 # we test for different critical pressures
 # to investigate the influence of the stop valve effect
 #crit_pressures = ["0", "10", "20", "40"]
-crit_pressures = ["30",]
-t_factor = [2,]
-vol = [200, 250, 300,]
+crit_pressures = ["0", "10", "20", "30", "40"]
+t_factor = [1,]
+vol = [150, 200, 250, 300,]
 
 cases = [
     tilting_speeds,
@@ -28,15 +28,16 @@ def run_case(case):
 
     args = params.DEFAULT_CASE_VALIDATION
     args["Problem.CapillaryStopPressure"] = crit_pressure
-    dryingT = 500.0 # micron
+    dryingT = 700.0 # micron
     args["Problem.DryingThresholdInMilliMeter"] = f"{dryingT/1000.0}"
     args["Problem.RotationsPerMinute"] = speed
     args["Problem.ChannelTransmissibility"] = str(float(tf)*float(args["Problem.ChannelTransmissibility"]))
     args["Problem.InitialVolumeInMicroLiter"] = str(vol)
+    args["TimeLoop.Dt"] = "0.05"
 
     cmd = make_cmd(args, f"validation_s{speed}_p{crit_pressure}_d{int(dryingT)}_{vol}ul_t{tf}_big.txt")
     subprocess.run(cmd)
 
 if __name__ == '__main__':
-    with mp.Pool(8) as p:
+    with mp.Pool(64) as p:
         p.map(run_case, itertools.product(*cases))
