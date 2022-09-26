544adf87aa8469de82f82796c0a7bf23a760e788
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2022-09-27 00:49:52 +0200

    Update plot scripts

--- appl/tilting-chip/plot_validation_water.py
+++ appl/tilting-chip/plot_validation_water.py
@@ -3,23 +3,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib.gridspec as gridspec
 
 def running_mean(x, N):
     cumsum = np.cumsum(np.insert(x, 0, 0))
     return (cumsum[N:] - cumsum[:-N]) / float(N)
 
-all_data = np.genfromtxt("../../../data/flow_water/data_water_tilt.csv", delimiter=",", dtype=np.float64)
+all_data = np.genfromtxt("../../../data/flow_water/data_water_speed.csv", delimiter=",", dtype=np.float64)
 data = [
-    np.genfromtxt("../../../data/flow_water/3rpm_300ul_t19_cellmedia.csv", delimiter=",", dtype=np.float64),
-    np.genfromtxt("../../../data/flow_cellmedia/5rpm_300ul_t19_cellmedia.csv", delimiter=",", dtype=np.float64),
-    np.genfromtxt("../../../data/flow_cellmedia/8rpm_300ul_t19_cellmedia.csv", delimiter=",", dtype=np.float64),
+    all_data[:, [0, i]] for i in range(1, all_data.shape[1])
 ]
 
 data_structured = {}
-speed = [3, 5, 8]
-for d, label in zip(data, speed):
+tilt_angle = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+speed = [3, 4, 5, 6, 7, 8, 9, 10]
+varied_param_str = "speed"
+varied_param = speed
+
+for d, label in zip(data, varied_param):
     t = d[:,0]
     f = d[:,1]
+
+    t = t[t < 60.0/float(label)]
+    f = f[:len(t)]
 
     # compute normalization using integral under curve which should be 300µl
     np.nan_to_num(f, copy=False)
@@ -40,22 +46,25 @@ def plot_measurement(ax, label, shift=4.0, everynth=1, vol=300):
     ax.plot(
         data_structured[label][vol][0][2:-2][::everynth],
         np.roll(running_mean(data_structured[label][vol][1], 5), shift)[::everynth],
-        "-",  label=f"Meas",
+        "-",  label=f"Measurement",
         ms=2*72.0/fig.dpi,
         alpha=0.5,
     )
     ax.set_xlim([0, 60.0/float(label)])
 
 
-def plot_simulation_speed(ax, label):
+def plot_simulation_speed(ax, label, vol=300):
     #for crit_pressure in ["0", "10", "20", "30", "40"][::-1]:
-    for crit_pressure in ["40", "80"][::-1]:
+    for crit_pressure in ["0",][::-1]:
     #for vol in [150, 200, 250, 300,]:
     #for vol in [250,]:
-        vol = 250
-        file_name = f"validation_s{label}_p{crit_pressure}_d700_{vol}ul_t1_big.txt"
-        label_str = f"S-{crit_pressure}Pa"
-        #label_str = f"Sim"
+        if varied_param_str == "speed":
+            file_name = f"validation_s{label}_a19_p{crit_pressure}_d700_{vol}ul_t1_big.txt"
+        else:
+            file_name = f"validation_s3_a{label}_p{crit_pressure}_d700_{vol}ul_t1_big.txt"
+
+        #label_str = f"S-{crit_pressure}Pa"
+        label_str = f"Simulation"
         if Path(file_name).is_file():
             print(file_name)
             sim = np.genfromtxt(file_name, delimiter=" ", skip_header=1, skip_footer=2)
@@ -67,38 +76,28 @@ def plot_simulation_speed(ax, label):
     #ax.plot(sim[:,0], sim[:,5], "-", label=label_str, ms=72.0/fig.dpi, color=p0.get_color(), alpha=0.2)
 
 
-fig, axes = plt.subplots(1, 2, figsize=(12,4), dpi=150, sharey=True, gridspec_kw={'width_ratios': [1, 3.0/8.0]})
+fig = plt.figure()
+gs0 = gridspec.GridSpec(2, 1, figure=fig)
+gs00 = gs0[0].subgridspec(1, 1)
+gs01 = gs0[1].subgridspec(1, 2, width_ratios=[1, 7.0/9.0])
+
+#fig, axes = plt.subplots(1, 2, figsize=(12,4), dpi=150, sharey=True, gridspec_kw={'width_ratios': [1, 4.0/7.0]})
 #ax0, ax1, ax2, ax3 = axes
-ax0, ax2 = axes
+ax0 = fig.add_subplot(gs00[0])
+ax1 = fig.add_subplot(gs01[0])
+ax3 = fig.add_subplot(gs01[1], sharey=ax1)
+plt.setp(ax3.get_yticklabels(), visible=False)
 offset = 0.0
 everynth = 10
 
-plot_simulation_speed(ax0, "3")
-#plot_measurement(ax0, "3", shift=18.0+offset, everynth=everynth, vol=300)
-plot_measurement(ax0, "3", shift=0.5+offset, everynth=everynth, vol=250)
-#plot_measurement(ax0, "3", shift=18.0+offset, everynth=everynth, vol=200)
-#plot_measurement(ax0, "3", shift=18.0+offset, everynth=everynth, vol=150)
+plot_simulation_speed(ax3, "9", vol=200)
+plot_measurement(ax3, "9", shift=15.0, everynth=everynth, vol=200)
 
-# plot_simulation_speed(ax1, "5")
-# #plot_measurement(ax1, "5", shift=19.5+offset, everynth=everynth, vol=300)
-# plot_measurement(ax1, "5", shift=19.5+offset, everynth=everynth, vol=250)
-# #plot_measurement(ax1, "5", shift=19.5+offset, everynth=everynth, vol=200)
-# #plot_measurement(ax1, "5", shift=19.5+offset, everynth=everynth, vol=150)
+plot_simulation_speed(ax0, "4", vol=200)
+plot_measurement(ax0, "4", shift=2.4, everynth=everynth, vol=200)
 
-plot_simulation_speed(ax2, "8")
-#plot_measurement(ax2, "8", shift=11.0+offset, everynth=everynth, vol=300)
-plot_measurement(ax2, "8", shift=12.7+offset, everynth=everynth, vol=250)
-#plot_measurement(ax2, "8", shift=11.0+offset, everynth=everynth, vol=200)
-#plot_measurement(ax2, "8", shift=11.0+offset, everynth=everynth, vol=150)
-
-# plot_simulation_speed(ax3, "3")
-# plot_measurement(ax3, "3", shift=18.0+offset, everynth=everynth)
-
-# plot_simulation_speed(ax3, "5")
-# plot_measurement(ax3, "5", shift=19.5+offset, everynth=everynth)
-
-# plot_simulation_speed(ax3, "8")
-# plot_measurement(ax3, "8", shift=11.0+offset, everynth=everynth)
+plot_simulation_speed(ax1, "7", vol=200)
+plot_measurement(ax1, "7", shift=12.0, everynth=everynth, vol=200)
 
 def set_size(w,h, ax=None):
     """ w, h: width, height in inches """
@@ -111,11 +110,14 @@ def set_size(w,h, ax=None):
     figh = float(h)/(t-b)
     ax.figure.set_size_inches(figw, figh)
 
-set_size(6, 6, ax0)
-set_size(6, 6*3.0/8.0, ax2)
+set_size(4, 4, ax0)
+set_size(4, 4, ax1)
+#set_size(8, 4*0.5, ax2)
+set_size(4, 4, ax3)
 
-for ax in axes.flatten():
+for ax in [ax0, ax1, ax3]:
     ax0.set_ylabel("Q in µl/s")
+    ax1.set_ylabel("Q in µl/s")
     ax.set_xlabel("t in s")
     ax0.legend(frameon=False)
 fig.tight_layout()
--- appl/tilting-chip/plot_wss_3d.py
+++ appl/tilting-chip/plot_wss_3d.py
@@ -4,40 +4,77 @@ import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib
 
 tilting_speeds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-reservoir = ["small", "big"]
 #volume = np.linspace(100, 800, endpoint=True, num=10)
 angles = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+#reservoir = "small"
+reservoir = "big"
 
 X, Y = np.meshgrid(tilting_speeds, angles)
 
 def max_wss(speed, tilt):
     def generate():
         for s, t in zip(speed, tilt):
-            #file_name = f"wss_s{s}_t{t}_p0_300ul_d500_big.txt"
-            file_name = f"wss_s{s}_t{t}_p0_200ul_d500_small.txt"
+            if reservoir == "big":
+                file_name = f"wss_s{s}_t{t}_p0_300ul_d500_big.txt"
+            else:
+                file_name = f"wss_s{s}_t{t}_p0_200ul_d500_small.txt"
             print(f"Parse {file_name}")
             if Path(file_name).is_file():
                 sim = np.genfromtxt(file_name, delimiter=" ", skip_header=1, skip_footer=2)
                 yield np.max(sim[:,6])*10 # Pa to dyne/cm^2
-                #yield np.max(-sim[:,4])
+            else:
+                yield 0
+    return np.array(list(generate()))
+
+def directionality(speed, tilt):
+    def generate():
+        for s, t in zip(speed, tilt):
+            if reservoir == "big":
+                file_name = f"wss_s{s}_t{t}_p0_300ul_d500_big.txt"
+            else:
+                file_name = f"wss_s{s}_t{t}_p0_200ul_d500_small.txt"
+            print(f"Parse {file_name}")
+            if Path(file_name).is_file():
+                sim = np.genfromtxt(file_name, delimiter=" ", skip_header=1, skip_footer=2)
+                yield np.trapz(np.abs(sim[:,4]), dx=sim[:,0][2]-sim[:,0][1])/300.0 # Pa to dyne/cm^2
             else:
                 yield 0
     return np.array(list(generate()))
 
 Z = max_wss(X.flatten(), Y.flatten()).reshape(X.shape)
+Z2 = directionality(X.flatten(), Y.flatten()).reshape(X.shape)
+
+color_dimension = Z2 # change to desired fourth dimension
+minn, maxx = color_dimension.min(), color_dimension.max()
+norm = matplotlib.colors.Normalize(minn, maxx)
+m = plt.cm.ScalarMappable(norm=norm, cmap='viridis')
+m.set_array([])
+fcolors = m.to_rgba(color_dimension)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                cmap='viridis', edgecolor='none')
-#ax.set_title('Maximum WSS (outer circuit | 300µl)')
-ax.set_title('Maximum WSS (inner circuit | 200µl)')
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                edgecolor='none', facecolors=fcolors, vmin=minn, vmax=maxx)
+
+if reservoir == "big":
+    ax.set_title('Maximum WSS (outer circuit / V=300µl)')
+else:
+    ax.set_title('Maximum WSS (inner circuit / V=200µl)')
 ax.set_xlabel('tilting speed (rpm)')
 ax.set_ylabel('tilt angle (degree)')
 ax.set_zlabel(r'Maximum WSS (dyne/cm$^{2}$)')
-ax.set_zlim(0, 8)
+
+if reservoir == "big":
+    ax.set_zlim(0, 12)
+else:
+    ax.set_zlim(0, 8)
+ax.view_init(elev=20, azim=-142)
+cbar = fig.colorbar(surf, shrink=0.5, aspect=10)
+cbar.set_label(r"$V\,/\,\int \! Q \,dt$")
 fig.tight_layout()
 plt.show()
 fig.savefig("wss_wireframe.pdf", dpi=600)
